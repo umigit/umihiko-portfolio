edbedd41f8eebfad21bfbc7caf725133f24db5ba
umigit <umi-amanuma@die-katze.net>
2021-08-17 22:43:28 +0900

    [add]add style

---
+++ portfolio/src/styles/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  orange: 'rgb(255,69,0)',
+  darkblue: '#003246',
+  lightblue: '#0387B1',
+  white: 'rgb(245,245,245)',
+  gray: 'rgb(180,180,180)',
+  alphaBlack: 'rgba(0, 0, 0, 0.6)',
+};
