2e7a27dcf1117a469b3011c81fc245d0f1a55503
umigit <umi-amanuma@die-katze.net>
2021-08-22 19:23:26 +0900

    [update]change layout

--- portfolio/src/components/layout.tsx
+++ portfolio/src/components/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'modern-css-reset';
 import { css } from '@emotion/react';
 import { StaticImage } from 'gatsby-plugin-image';
+import { use100vh } from 'react-div-100vh';
 import { Header } from './header';
 import { MobileNav } from './mobile-nav';
 import { useMediaQuery } from 'react-responsive';
@@ -43,10 +44,11 @@ export const Layout: React.FC<{ pathname: string }> = ({
   const isMobile = useMediaQuery(moblie);
   const isTablet = useMediaQuery(tablet);
   const isPC = useMediaQuery(PC);
+  const minHeight = use100vh() || '100vh';
 
   return (
     <>
-      <div css={main}>
+      <div css={main} style={{ minHeight: minHeight }}>
         <StaticImage
           css={image}
           src='../images/bruno_bg.jpg'
