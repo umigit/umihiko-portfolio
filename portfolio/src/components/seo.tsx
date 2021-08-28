139627b94b018601f27a1d1ead548ea1f8e16b83
umigit <umi-amanuma@die-katze.net>
2021-08-29 01:00:55 +0900

    [update]add meta

--- portfolio/src/components/seo.tsx
+++ portfolio/src/components/seo.tsx
@@ -24,15 +24,17 @@ export const SEO: React.FC<Props> = ({
   const metaDescription = description || site?.siteMetadata?.description;
   const metaImage = image ? image : site?.siteMetadata?.image;
   const metaUrl = `${site?.siteMetadata?.url}${pathname}`;
-  const metaArticle = article || false;
+  const metaType = article ? 'article' : 'website';
 
   return (
     <Helmet title={metaTitle!}>
       <html prefix='og: http://ogp.me/ns#' />
       <meta name='description' content={metaDescription!} />
       <meta name='image' content={metaImage!} />
+      <meta property='og:site_name' content='Umihiko' />
+      <meta property='og:type' content={metaType} />
       {metaUrl && <meta property='og:url' content={metaUrl!} />}
-      {metaArticle && <meta property='og:type' content='article' />}
+
       {metaTitle && <meta property='og:title' content={metaTitle} />}
       {metaDescription && (
         <meta property='og:description' content={metaDescription} />
