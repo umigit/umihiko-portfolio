260945784d7a8559d85ad576d01c17c0a4c925ef
umigit <umi-amanuma@die-katze.net>
2021-08-29 16:04:47 +0900

    [update]change gatsby-ssr.js

--- portfolio/gatsby-ssr.js
+++ portfolio/gatsby-ssr.js
@@ -1,19 +1,32 @@
-exports.onPreRenderHTML = function onPreRenderHTML({
-  getHeadComponents,
-  replaceHeadComponents,
-}) {
+const React = require('react');
+const { Helmet } = require('react-helmet');
+
+exports.onRenderBody = (
+  { setHeadComponents, setHtmlAttributes, setBodyAttributes },
+  pluginOptions
+) => {
+  const helmet = Helmet.renderStatic();
+  setHtmlAttributes(helmet.htmlAttributes.toComponent());
+  setBodyAttributes(helmet.bodyAttributes.toComponent());
+  setHeadComponents([
+    helmet.title.toComponent(),
+    helmet.link.toComponent(),
+    helmet.meta.toComponent(),
+    helmet.noscript.toComponent(),
+    helmet.script.toComponent(),
+    helmet.style.toComponent(),
+  ]);
+};
+
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
   const headComponents = getHeadComponents();
-  headComponents.sort((a, b) => {
-    if (a.type === b.type || (a.type !== 'style' && b.type !== 'style')) {
-      return 0;
-    }
 
-    if (a.type === 'style') {
-      return 1;
-    } else if (b.type === 'style') {
+  headComponents.sort((x, y) => {
+    if (x.props && x.props['data-react-helmet']) {
       return -1;
+    } else if (y.props && y.props['data-react-helmet']) {
+      return 1;
     }
-
     return 0;
   });
 
